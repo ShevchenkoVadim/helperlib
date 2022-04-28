d0bcc1b21a380d841b37e7282c95b6f5ce3ec5ce
Vadym Shevchenko <vshevchenko.mne@gmail.com>
2022-04-28 17:36:08 +0200

    add config reader

---
+++ config/config.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"github.com/ShevchenkoVadim/helperlib/sfotypes"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var C = sfotypes.Configurations{}
+
+func init() {
+	log.Println("Load config file...")
+	configName := filepath.Base(os.Args[0])
+	f, err := ioutil.ReadFile(configName + ".yaml")
+
+	if err != nil {
+		log.Fatal("Cannot open config file")
+		os.Exit(101)
+	}
+
+	err = yaml.Unmarshal(f, &C)
+	if err != nil {
+		log.Fatal("Cannot read config file")
+		os.Exit(102)
+	}
+	log.Println(C)
+}
