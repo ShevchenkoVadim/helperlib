7720b69729d62e6f86f099dae60bb4255d6edf77
Vadym Shevchenko <vshevchenko.mne@gmail.com>
2022-05-14 13:09:22 +0300

    add Kata to config

--- sfotypes/conf.go
+++ sfotypes/conf.go
@@ -12,6 +12,7 @@ type Configurations struct {
 	SSLCert       Certificate  `yaml:"ssl_cert"`
 	MQ            MqConfig     `yaml:"mq"`
 	MS            MSConfig     `yaml:"ms"`
+	Kata          KataConfig   `yaml:"kata"`
 	DBConn        DBConnection `yaml:"db_conn"`
 	DebugEnable   bool         `yaml:"debugenable"`
 	Hostname      string       `yaml:"hostname"`
