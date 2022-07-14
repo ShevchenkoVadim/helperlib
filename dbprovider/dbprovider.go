47a4c109cea9a13b3eee9d41765d07d5447d8361
Vadym Shevchenko <vshevchenko.mne@gmail.com>
2022-07-14 11:42:31 +0300

    Fix logs text

--- dbprovider/dbprovider.go
+++ dbprovider/dbprovider.go
@@ -31,7 +31,7 @@ func (mgr *DBManager) writeToWaitChannel() {
 }
 
 func (mgr *DBManager) checkConnect() {
-	utils.LogWrapper("checkConnect")
+	utils.LogWrapper("check TCP connect to DB")
 	conn, err := net.DialTimeout("tcp",
 		fmt.Sprint(mgr.DbConn.Server, ":", mgr.DbConn.Port), time.Second)
 	if conn != nil {
@@ -40,20 +40,20 @@ func (mgr *DBManager) checkConnect() {
 	if err != nil {
 		utils.LogWrapper(err)
 		for {
-			utils.LogWrapper("checkConnect: Wait to connect")
+			utils.LogWrapper("check TCP connect to DB: wait to connect")
 			time.Sleep(time.Second * 2)
 			mgr.checkConnect()
 		}
 	} else {
-		utils.LogWrapper("checkConnect Ok")
+		utils.LogWrapper("check TCP connect to DB: connect is ok")
 		go mgr.writeToWaitChannel()
 	}
 }
 
 func (mgr *DBManager) Connect() {
-	utils.LogWrapper("DB Connect")
 	go mgr.checkConnect()
 	<-mgr.WaitChannel
+	utils.LogWrapper("DB is tried to connoent")
 	//uri := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&connection+timeout=%d",
 	//	mgr.DbConn.Server, mgr.DbConn.User, mgr.DbConn.Password, mgr.DbConn.Port, mgr.DbConn.Timeout, mgr.DbConn.Db)
 	uri := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&connection+timeout=%d",
@@ -64,13 +64,13 @@ func (mgr *DBManager) Connect() {
 	}
 	db, err := sql.Open("sqlserver", uri)
 	if err != nil {
-		log.Fatal("Failed to init db:", err)
+		log.Fatal("DB connection is failed: ", err)
 	}
 	mgr.db = db
 }
 
 func (mgr *DBManager) SearchFileName(filePath string, userName string) (bool, error) {
-	utils.LogWrapper("DB SearchFileName")
+	utils.LogWrapper("DB search file for user: ", userName, filePath)
 	var name string
 	var err error
 	for {
@@ -89,14 +89,14 @@ func (mgr *DBManager) SearchFileName(filePath string, userName string) (bool, er
 }
 
 func (mgr *DBManager) execWithPrepare(query string, args ...any) (*sql.Stmt, error) {
-	utils.LogWrapper("execWithPrepare")
+	utils.LogWrapper("DB execute query with prepare statement: ", query)
 	var stmt *sql.Stmt
 	var err error
 
 	for {
 		stmt, err = mgr.db.Prepare(query)
 		if err != nil {
-			log.Println("ERROR: ", err)
+			log.Println("DB error execute query: ", err)
 			go mgr.checkConnect()
 			<-mgr.WaitChannel
 			continue
@@ -109,13 +109,14 @@ func (mgr *DBManager) execWithPrepare(query string, args ...any) (*sql.Stmt, err
 
 func (mgr *DBManager) InsertFileName(filePath string, userName string) (*sql.Stmt, error) {
 	query := "INSERT INTO files (filename, username) VALUES (@p1, @p2)"
-	utils.LogWrapper(query)
+	utils.LogWrapper("DB insert file: ", query)
 	stmt, err2 := mgr.execWithPrepare(query, filePath, userName)
 	return stmt, err2
 }
 
 func (mgr *DBManager) DeleteFileName(filePath string, userName string) (*sql.Stmt, error) {
 	query := "DELETE FROM files WHERE filename=@p1 AND username=@p2"
+	utils.LogWrapper("DB delete file: ", query)
 	stmt, err2 := mgr.execWithPrepare(query, filePath, userName)
 	return stmt, err2
 }
